b80103d2beff00790e234e550d5fdfd8d01ef870
AndTorGuer <atorresguerrero9@gmail.com>
2022-01-25 16:23:11 -0500

    feature0

--- Lab1_Andres/Program.cs
+++ Lab1_Andres/Program.cs
@@ -6,7 +6,25 @@ namespace Lab1_Andres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            static void showMenu() //used method to print a text menu for user to see
+            {
+                Console.WriteLine("\t\tMain Menu\n"); //use of \t to tabulate string and \n to jump to next line and make it more appealing
+                Console.WriteLine("Make your selection from the following options:");
+                Console.WriteLine("(To make selection just enter the number)\n"); //use of \n to jump to next line and make it more appealing
+                Console.WriteLine("Option 1: Show all grades");
+                Console.WriteLine("Option 2: Add grades");
+                Console.WriteLine("Option 3: Get grade average from class");
+                Console.WriteLine("Option 4: Show top grade");
+                Console.WriteLine("Option 5: Show bottom grade");
+                Console.WriteLine("Option 6: Remove student grade");
+                Console.WriteLine("Option 7: Edit student grade");
+                Console.WriteLine("Option 8: Exit");
+            }
+
+
+
+
         }
     }
 }
